0cf9e903567e9714a091e384f799f41995f43bf8
Mateo Patino <96802084+mateo-patino@users.noreply.github.com>
2026-01-09 13:07:53 -0500

    Plotting scripts intial commit

---
+++ speed_frequency/plot_array.py
@@ -0,0 +1,36 @@
+"""
+
+Author(s): Mateo Patino, Hod Lipson
+Creative Machines Laboratory, Columbia University
+
+I use this script for plotting a "POSITION" array from a numpy binary file; useful for doing a quick
+sanity check on a simulation.
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# makes a  x and y
+def linePlot(x, y, title, xlabel, ylabel):
+    plt.plot(x, y, color="tab:red", zorder=1)
+    plt.scatter(x[:1], y[:1], color="black", marker="d", label="Start")
+    plt.scatter(x[len(x)-1:], y[len(y)-1:], color="black", marker="x", label="End")
+    plt.legend()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid("both")
+    plt.axis("equal")
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    file = "1l_10h_1.npy"
+    POSITION = np.load("data/" + file)
+
+    x = POSITION[:, 0]
+    y = POSITION[:, 1]
+
+    linePlot(x, y, title=f"{file}", xlabel="x (m)", ylabel="y (m)")
---
+++ speed_frequency/plot_multiple_arrays.py
@@ -0,0 +1,47 @@
+"""
+
+Author(s): Mateo Patino, Hod Lipson
+Creative Machines Laboratory, Columbia University
+
+I use this script for plotting multiple numpy arrays collected from the simulations. This script 
+uses a grid with user-defined rows and columns to plot many arrays in multiple axes.
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# makes a  x and y
+def linePlot(ax, x, y, title, xlabel, ylabel):
+    ax.plot(x, y, color="tab:red", zorder=1)
+    ax.scatter(x[:1], y[:1], color="black", marker="d", label="Start")
+    ax.scatter(x[len(x)-1:], y[len(y)-1:], color="black", marker="x", label="End")
+    ax.legend()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.grid("both")
+    ax.axis("equal")
+
+
+if __name__ == "__main__":
+
+    files = ["0l_10h_0.npy", "0l_10h_1.npy", "0l_10h_2.npy",
+             "0l_10h_0.npy", "0l_10h_1.npy", "0l_10h_2.npy",]
+    rows = 2
+    columns = 3
+    fig, axes = plt.subplots(rows, columns)
+
+    fileCount = 0
+    for r in range(rows):
+        for c in range(columns):
+            file = files[fileCount]
+            fileCount += 1
+            POSITION = np.load("data/" + file)
+            x = POSITION[:, 0]
+            y = POSITION[:, 1]
+            linePlot(axes[r][c], x, y, title=f"{file}", xlabel="x (m)", ylabel="y (m)")
+
+    plt.show()
+
+    

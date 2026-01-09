00608ae8481ffeb1e7b043fe070b776a48a39e46
Mateo Patiño Hasbon <mateopatinohasbon@MacBook-Pro-1184.local>
2026-01-08 22:02:24 -0500

    Added Google Colab runtime code

---
+++ colab/plot_array.py
@@ -0,0 +1,38 @@
+""""
+
+Author(s): Mateo Patino, Hod Lipson
+Creative Machines Laboratory, Columbia University
+
+I use this script for plotting a "POSITION" array from a numpy binary file; useful for doing a quick
+sanity check on a simulation.
+
+"""
+
+from math import sqrt
+import matplotlib.pyplot as plt
+import numpy as np
+from sys import argv
+
+# makes a  x and y
+def linePlot(x, y, title, xlabel, ylabel):
+    plt.plot(x, y, color="tab:red", zorder=1)
+    plt.scatter(x[:1], y[:1], color="black", marker="d", label="Start")
+    plt.scatter(x[len(x)-1:], y[len(y)-1:], color="black", marker="x", label="End")
+    plt.legend()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid("both")
+    plt.axis("equal")
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    file = "49p_1.npy"
+    POSITION = np.load("data/" + file)
+
+    x = POSITION[:, 0]
+    y = POSITION[:, 1]
+
+    linePlot(x, y, title=f"{file}", xlabel="x (m)", ylabel="y (m)")

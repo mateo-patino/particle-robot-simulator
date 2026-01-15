e827e9cadd5c7300e5b65cf5c26c3e60b84057a1
Mateo Patino <96802084+mateo-patino@users.noreply.github.com>
2026-01-15 15:20:32 -0500

    Fixed zorder

--- speed_frequency/plot_multiple_arrays.py
+++ speed_frequency/plot_multiple_arrays.py
@@ -13,9 +13,9 @@ import numpy as np
 
 # makes a  x and y
 def linePlot(ax, x, y, title, xlabel, ylabel):
-    ax.plot(x, y, color="tab:red", zorder=1)
-    ax.scatter(x[:1], y[:1], color="black", marker="d", label="Start")
-    ax.scatter(x[len(x)-1:], y[len(y)-1:], color="black", marker="x", label="End")
+    ax.plot(x, y, color="tab:red", zorder=2)
+    ax.scatter(x[:1], y[:1], color="black", marker="d", label="Start", zorder=3)
+    ax.scatter(x[len(x)-1:], y[len(y)-1:], color="black", marker="x", label="End", zorder=3)
     ax.legend()
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -26,10 +26,11 @@ def linePlot(ax, x, y, title, xlabel, ylabel):
 
 if __name__ == "__main__":
 
-    geomType = "sphere"
-    files = ["1l_10h_1.npy", "1l_10h_2.npy", "1l_10h_3.npy",
-             "5l_10h_1.npy", "5l_10h_2.npy", "5l_10h_3.npy",
-             "10l_10h_1.npy", "10l_10h_2.npy", "10l_10h_3.npy",]
+    geomType = "cylinder"
+    low = 1
+    files = [f"{low}l_10h_9.npy", f"{low}l_10h_21.npy", f"{low}l_10h_30.npy",
+             f"{low}l_10h_8.npy", f"{low}l_10h_23.npy", f"{low}l_10h_27.npy",
+             f"{low}l_10h_4.npy", f"{low}l_10h_6.npy", f"{low}l_10h_11.npy",]
     rows = 3
     columns = 3
     fig, axes = plt.subplots(rows, columns)

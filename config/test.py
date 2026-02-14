cbf06c2f1d9a6380c1029391b5ba0b2ae69c2169
Mateo Patino <96802084+mateo-patino@users.noreply.github.com>
2026-02-14 13:23:19 -0500

    Removed test files

--- config/test.py
+++
@@ -1,6 +0,0 @@
-from config import SimulationConfig
-
-c = SimulationConfig.from_json("default.json")
-
-print(c.rotor_mass)
-print(c.to_dict())
--- simulator/simulation/test.py
+++
@@ -1,8 +0,0 @@
-class Test:
-    def __inti__(self):
-        print("Initialized")
-
-    def method1(self):
-        print("This is method 1, calling upon method 2")
-        self.method2()
-

799d1fbdd46392501c64d60337bdb8d635e2f5c2
Mateo Patino <96802084+mateo-patino@users.noreply.github.com>
2026-02-13 23:19:37 -0500

    Added chain and missing particle parameters

--- config/config.py
+++ config/config.py
@@ -14,6 +14,9 @@ class SimulationConfig:
     particle_mass: float = 0.26
     rotor_mass: float = 0.056
     geom_type: str = "sphere"
+    particle_solref: list = [0.003, 1]
+    particle_solimp: list = [0.95, 0.95, 0.005]
+    particle_clearance: float = 1e-05
 
     # Gradient parameters
     high_freq: float = 10
@@ -21,6 +24,7 @@ class SimulationConfig:
     phase_noise_std: float = 0.05
 
     # Simulation parameters
+    size: int = 64
     sim_duration: float = 15
     timestep: float = 7e-05
     solver: str = "Newton"
@@ -30,6 +34,10 @@ class SimulationConfig:
     # Chain parameters
     links_per_side: int = 5
     tau: float = 0.7
+    link_radius: float = 0.019
+    link_mass: float = 0.0155
+    link_solref: list = [0.003, 1]
+    link_solimp: list = [0.9, 0.9, 0.005]
 
     # Control parameters
     target_direction: tuple = (1, 0)

eb464e3f275ce9d5ad1562ee3d9f0982863d974f
Mateo Patino <96802084+mateo-patino@users.noreply.github.com>
2026-02-15 14:14:24 -0500

    Implemented: do multiple runs per experiment

--- simulator/experiments/single_run.py
+++ simulator/experiments/single_run.py
@@ -1,5 +1,13 @@
 from simulator.simulation.engine import Simulation
 
-def run_single(config):
-    sim = Simulation(config)
-    return sim.run()
+"""
+Parameters: a number of runs and a configuration object
+Returns: a dictionary of the results of the simulation
+"""
+
+def run_single(config, num_runs):
+    results = {}
+    for run in range(1, num_runs + 1):
+        sim = Simulation(config)
+        results[run] = sim.run()
+    return results
--- simulator/experiments/sweep.py
+++ simulator/experiments/sweep.py
@@ -4,30 +4,44 @@ from copy import deepcopy
 """
 
 Parameters: configuration, parameter to sweep, values to sweep over.
-Returns: A Python dictionary where they keys correspond to each value in the sweeping space
-and the value corresponds to the c.o.m array returned by the simulation.
+Returns: A 2D Python dictionary where the first dimension of keys corresponds to the value
+in the parameter space, and the second dimension of keys corresponds to the run number.
 
 """
 
-def run_1d_sweep(config, target_parameter, values=None):
+def run_1d_sweep(config, target_parameter, values=None, num_runs=1):
+
     if values is None or len(values) == 0:
-        raise ValueError(" A non-empty set of values to sweep must be provided.")
+        raise ValueError("A non-empty set of values to sweep must be provided.")
     
     if not hasattr(config, target_parameter):
-        raise ValueError(f"{target_parameter} is not a valid configuration parameter.")
+        raise ValueError(f"Cannot sweep \"{target_parameter}\": not a valid configuration parameter.")
     
     print(f"\nSweeping \"{target_parameter}\" over the values {values}")
 
-    results = dict()
+    sweep_results = {}
     for i, value in enumerate(values, start=1):
+
         print("\n----------------------------------------------------------------")
         print(f"\nRunning {target_parameter} = {value} ({i} out of {len(values)})")
-        new_config = deepcopy(config)
-        setattr(new_config, target_parameter, value)
 
-        sim = Simulation(new_config)
-        results[str(value)] = sim.run()
+        results = {}
+        for run in range(1, num_runs + 1):
+
+            new_config = deepcopy(config)
+            setattr(new_config, target_parameter, value)
+
+            # Ensure variability across runs by shifting the seed by +1
+            if config.seed is not None:
+                new_config.seed = config.seed + run - 1
+
+            print(f"\nRun {run} started.........")
+            sim = Simulation(new_config)
+            results[run] = sim.run()
+            print(f"\nRun {run} complete")
+
+        sweep_results[str(value)] = results
 
     print("\nSweeping complete")
 
-    return results
+    return sweep_results
